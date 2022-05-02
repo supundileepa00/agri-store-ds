50ec81040206d9c910ad5137f7e5b4e3ba68f5b5
unknown <it20254384@my.sliit.lk>
2022-05-02 17:42:02 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5500;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB Connection Success!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Running on PORT : ${PORT}`);
+});
